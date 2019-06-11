0c1b844b46e22ce3ecb332313974322dbb2cf551
Paul Unsted <ed__84@hotmail.com>
2019-06-11 03:50:29 -0400

    Knocked out some basic javascript functionality involving randomization, and did some light html modification in order to execute that script

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,22 @@ var currentVal = 0;
 
 // copypasta'd this in so that it can be renamed and repurposed for random number generation, but this is referring to an array, which simply doesn't work for my needs in this case. Look to the lotterygen exercises for ways to rewrite this randomizer to wait the needs of the game.
 
-compGuess = alpha[Math.floor(Math.random() * alpha.length)];
+// solution = alpha[Math.floor(Math.random() * alpha.length)];
 
-// Next write a random number function that will set up the win/loss value.
+$(document).ready(function() {
+
+    $(".userChoice").on("click", function() {
+        
+        // Feel beyond free to remove this element. Alerts are great to halt code executions or run initial tests to confirm things, but beyond that they can slow down code executions.
+        
+        alert("I've been clicked!");
+
+        // Generates a new random number between 1 and 12. Really appreciating the elegance of these statements.
+        var random = Math.floor(Math.random(0) * 13);
+
+        console.log(random);
+
+        // Yep, it works. Next step is to rename that variable random1 and then write random2 to random 4. Also it's worth considering moving these vars up into global scope. Not sure if it makes a difference if they are defined before or after document ready, but that's a question for another time. At this point, consolidate all the variables together for purposes of organization.
+
+    });
+});
